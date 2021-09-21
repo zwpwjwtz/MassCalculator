614d2a06ab9f23605e1165771f3339a5a0a3e1a6
zwpwjwtz <zwpwjwtz@126.com>
2021-09-21 11:18:18 +0800

    Specify the version number, and adjust the background of the main icon.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -36,6 +36,19 @@ void MainWindow::changeEvent(QEvent *e)
         default:
             break;
     }
+
+    ui->labelVersion->setText(QString(tr("Ver: %1")).arg(APP_VERSION));
+    ui->textAbout->setPlainText(
+                tr("MassCalculator - A simple tool for calculating molecular weight.\n\n"
+                   "This program is a free software.\n\n"
+                   "You can redistribute it and/or modify it under the terms of "
+                   "the GNU Library General Public License as published by "
+                   "the Free Software Foundation; either version 3 of the License, "
+                   "or (at your option) any later version.\n\n"
+                   "This program is distributed in the hope that it will be useful, but WITHOUT "
+                   "ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or "
+                   "FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License "
+                   "for more details.\n"));
 }
 
 void MainWindow::showAllowedElementRanges()
